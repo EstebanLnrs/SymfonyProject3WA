d8b8c181cf4a94b9454e1505256807edb83a9e56
EstebanLnrs <esteb-62@hotmail.fr>
2024-03-07 12:39:49 +0100

    adding auth + email verification + services + little changes

---
+++ migrations/Version20240306090703.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240306090703 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, is_verified TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE fighter ADD CONSTRAINT FK_7A08C3FCD262AF09 FOREIGN KEY (resume_id) REFERENCES score (id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_7A08C3FCD262AF09 ON fighter (resume_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE user');
+        $this->addSql('ALTER TABLE fighter DROP FOREIGN KEY FK_7A08C3FCD262AF09');
+        $this->addSql('DROP INDEX UNIQ_7A08C3FCD262AF09 ON fighter');
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,39 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker;
 
 class AppFixtures extends Fixture
 {
+    
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = \Fake\Factory::create('fr_FR');
+        $fighter = [];
+        $organisation = [];
+        $categories = [];
 
-        $manager->flush();
+        for ($i = 0; $i < 30; $i++) {
+            $organisation = new Organisation();
+            $organisation->setName($faker->word);
+            $organisation[] = $organisation;
+            $manager->persist($organisation);
+        }
+
+        for ($i = 0; $i < 30; $i++) {
+            $categories = new Categories();
+            $categories->setName($faker->word);
+            $categories[] = $categories;
+            $manager->persist($organisation);
+        }
+
+        for ($i = 0; $i < 30; $i++) {
+            $categories = new Categories();
+            $categories->setName($faker->word);
+            $categories[] = $categories;
+            $manager->persist($organisation);
+        }
+
     }
 }
